db8c6ef523c45ddbd2e794a7a65eb4abfa651678
Sagar <sagar@Sagars-MacBook-Air.local>
2018-09-22 00:33:11 +0530

    index.js updated - checking querystring values

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
 const http = require('http');
+const url = require('url');
 
 const app = http.createServer((request,response) => {
-  console.log('server is running...')
+    var querystring
+    querystring = url.parse(request.url,true).query;
+    if (querystring) {
+        response.writeHead(200, {"Content-Type": "text/html"});
+        console.log('the index.js has loaded')
+        response.write(`<h1>The parsed value from the url is ${querystring.value}</h1>`);
+        response.end();
+    }
+    
+    else {
+        response.write(`<h1>No values were passed</h1>`);
+        response.end();
+    }
+
 })
 
 app.listen(3000);
