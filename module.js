5cb1d74144adf223410f27a2e1f5fd9975b0ada6
Sagar <sagar@Sagars-MacBook-Air.local>
2018-09-22 00:42:52 +0530

    added cities module and updated module.js

--- module.js
+++ module.js
@@ -1,4 +1,7 @@
-/* module.js */
+const cities = require('cities');
+
 exports.addlog = () => {
     console.log(` module.js has loaded`)
+    var myCity = cities.zip_lookup(10016);
+    console.log(myCity);
 }
